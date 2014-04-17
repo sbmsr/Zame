26887e6a4c5162b6a9f609289530fd582628524d
SM <messs@seas.upenn.edu>
2014-04-17 17:18:41 -0400

    init commit

---
+++ Zame/Zame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Zame
+//
+//  Created by Sebastian Messier on 4/17/14.
+//  Copyright (c) 2014 CIS195. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Zame/ZameTests/ZameTests.m
@@ -0,0 +1,34 @@
+//
+//  ZameTests.m
+//  ZameTests
+//
+//  Created by Sebastian Messier on 4/17/14.
+//  Copyright (c) 2014 CIS195. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZameTests : XCTestCase
+
+@end
+
+@implementation ZameTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
